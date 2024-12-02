2d88e203bf501104934ae152cde50395f56d426f
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-12-02 15:04:18 +0100

    Basic language cards

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -4,6 +4,7 @@ export default function Main() {
   return (
     <main>
       <div className="container">
+        {/* BUTTONS */}
         <div className="buttons-container p-3">
           {languages.map((language) => (
             <button key={language.id} className="btn btn-primary me-2">
@@ -11,6 +12,14 @@ export default function Main() {
             </button>
           ))}
         </div>
+
+        {/* LANGUAGE CARDS */}
+        {languages.map((language) => (
+          <div key={language.id} className="card p-3 m-3">
+            <div className="card-title fw-bold">{language.title}</div>
+            <div className="card-description">{language.description}</div>
+          </div>
+        ))}
       </div>
     </main>
   );

e7b8745ad52ec818a6c3be3c000e9b26576b2311
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-12-02 14:45:44 +0100

    Layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
+import Header from "./components/layout/Header";
+import Main from "./components/layout/Main";
+import Footer from "./components/layout/Footer";
+
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello world</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
